14d3ce0979d6d7a66c0a16f72aea5b00bfc1bd2d
Mohammed Ahmed <AhmedMA10@cardiff.ac.uk>
2023-02-22 14:58:07 +0000

    added puzzles table and attempts table

--- advent_of_code/models.py
+++ advent_of_code/models.py
@@ -1,17 +1,19 @@
-from advent_of_code import db, login_manager
-from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
+from werkzeug.security import check_password_hash, generate_password_hash
+
+from advent_of_code import db, login_manager
+
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
-
     username = db.Column(db.String(20), unique=True,nullable=False)
     email_address = db.Column(db.String(120), unique=True, nullable=False)
     password_hash = db.Column(db.String(128), nullable=False)
     score = db.Column(db.Integer, nullable=False, default=0)
+    attempts = db.relationship("Attempt", backref="user")
 
     def __repr__(self):
-        return f"User(id:'{self.id}', username:'{self.username}')"
+        return f"User(id:'{self.id}', username:'{self.username}', email_address:'{self.email_address}', score:'{self.score}')"
     
     @property
     def password(self):
@@ -23,6 +25,27 @@ class User(db.Model, UserMixin):
     
     def verify_password(self, password):
         return check_password_hash(self.password_hash, password)
+
+class Puzzle(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    puzzle = db.Column(db.String(1000), nullable=False)
+    date = db.Column(db.DateTime, nullable=False)
+    answer = db.Column(db.String(1000), nullable=False)
+    attempts = db.relationship("Attempt", backref="puzzle")
+
+    def __repr__(self):
+        return f"Puzzles(id:'{self.id}', puzzle:'{self.puzzle}', date:'{self.date}', answer:'{self.answer}')"
+
+class Attempt(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    puzzle_id = db.Column(db.Integer, db.ForeignKey('puzzles.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    attempt_data = db.Column(db.String(1000), nullable=False)
+    date = db.Column(db.DateTime, nullable=False)
+    score = db.Column(db.Integer, nullable=False)
+
+    def __repr__(self):
+        return f"Attempt(id:'{self.id}', puzzle_id:'{self.puzzle_id}', attempt_data:'{self.attempt_data}', date:'{self.date}', score:'{self.score}')"
 
 @login_manager.user_loader
 def load_user(user_id):
